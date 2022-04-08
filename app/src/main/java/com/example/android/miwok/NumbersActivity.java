4768b68429bba15c32456fe786c04940470a9c97
Serban Patriciu <serban_patriciu@hotmail.com>
2022-04-08 23:54:50 +0300

    Add TextViews with the help of a while loop.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,14 +1,40 @@
 package com.example.android.miwok;
 
+import static java.util.Arrays.asList;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
+
+    private static String TAG = "NumbersActivity";
+
+    ArrayList<String> words;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        words = new ArrayList<>(asList("one", "two", "three",
+                "four", "five", "six", "seven", "eight", "nine", "ten"));
+
+        LinearLayout rootView = findViewById(R.id.rootView);
+
+        int index = 0;
+        while (index < 10) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+
+            index += 1;
+        }
     }
+
 }
