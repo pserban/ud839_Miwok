42fbc926e3350a38ad60466952ac4226a10cb500
Serban Patriciu <serban_patriciu@hotmail.com>
2022-04-08 15:43:29 +0300

    Add code to start the Numbers activity using Click listeners.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,7 +15,10 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 // import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,5 +30,23 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        NumbersClickListener clickListener = new NumbersClickListener();
+
+        // Find the View that shows the numbers category
+        TextView numbers = findViewById(R.id.numbers);
+
+        // Set a clickListener on that View
+        numbers.setOnClickListener(clickListener);
     }
+
+    /**
+     * Starts the NumbersActivity
+     * @param view
+     */
+    public void openNumbersList(View view) {
+        Intent intent = new Intent(this, NumbersActivity.class);
+        startActivity(intent);
+    }
+
 }
