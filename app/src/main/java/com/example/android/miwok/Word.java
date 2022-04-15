992590e6026984a29d6eef48de4f6f677aa613f3
Serban Patriciu <serban_patriciu@hotmail.com>
2022-04-15 10:50:27 +0300

    Implement logic to play corresponding audio file for each word or phrase.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,16 +4,19 @@ public class Word {
     private String mMiwokTranslation;
     private String mDefaultTranslation;
     private int mImageResourceID = NO_IMAGE_PROVIDED;
+    private int mAudioResourceID = NO_AUDIO_PROVIDED;
 
     private static final int NO_IMAGE_PROVIDED = -1;
+    private static final int NO_AUDIO_PROVIDED = -1;
 
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceID) {
         this.mMiwokTranslation = miwokTranslation;
         this.mDefaultTranslation = defaultTranslation;
+        this.mAudioResourceID = audioResourceID;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID) {
-        this(defaultTranslation, miwokTranslation);
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID, int audioResourceID) {
+        this(defaultTranslation, miwokTranslation,audioResourceID);
         mImageResourceID = imageResourceID;
     }
 
@@ -31,5 +34,13 @@ public class Word {
 
     public boolean hasImage() {
         return (mImageResourceID != NO_IMAGE_PROVIDED);
+    }
+
+    public int getAudioResourceID() {
+        return mAudioResourceID;
+    }
+
+    public boolean hasAudio() {
+        return (mAudioResourceID != NO_AUDIO_PROVIDED);
     }
 }
